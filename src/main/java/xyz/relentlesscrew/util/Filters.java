a4b54cfe37bf1774276332e780c7286d962fd2c7
Marko Medic <mrkmdc@gmail.com>
2018-06-22 06:34:35 +0200

    updated cors filter

--- src/main/java/xyz/relentlesscrew/util/Filters.java
+++ src/main/java/xyz/relentlesscrew/util/Filters.java
@@ -17,7 +17,11 @@ public class Filters {
     public static Filter CORSFilter = (request, response) -> {
         Map<String, String> CORSHeaders = Collections.unmodifiableMap(new HashMap<String, String>() { {
             put("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-            put("Access-Control-Allow-Origin", "*");
+            if (request.ip().equals(System.getenv("PHASE_IP"))) {
+                put("Access-Control-Allow-Origin", System.getenv("PHASE_IP"));
+            } else {
+                put("Access-Control-Allow-Origin", "http://relentlesscrew.xyz");
+            }
             put("Access-Control-Allow-Headers", "Content-Type,apiKey");
             put("Access-Control-Max-Age", "1800");
         }
